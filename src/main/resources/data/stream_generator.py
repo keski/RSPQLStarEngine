6566b77bfd1ee7e2f6a4d038b43d1ed1f88e310a
Robin Keskisärkkä <robin.keskisarkka@liu.se>
2019-05-02 01:20:05 +0200

    add stream generator

--- src/main/resources/data/stream_generator.py
+++ src/main/resources/data/stream_generator.py
@@ -46,52 +46,102 @@ activity_map = {
     }
 }
 
-def heart_rate(i, heart_rate_avg, t):
-    value = (1 + .2 * random.random()) * heart_rate_avg
-    time = datetime.utcfromtimestamp(t).strftime('%Y-%m-%d %H:%M:%S')
+def heart_rate(i, value, t):
+    value *= (1 + .2 * random.random())
+    time = datetime.utcfromtimestamp(t + i).strftime('%Y-%m-%dT%H:%M:%S')
     test = """
-_:{0} {{
-    <observation/{0}> a sosa:Observation ;
-        sosa:observedProperty <person1/heart_rate> ;
-        sosa:hasFeatureOfInterest <person1> ;
-        sosa:madeBySensor <sensor1> ;
-        sosa:resultTime "{1}"^^xsd:dateTime .
-    <<<observation/{0}> sosa:hasSimpleResult {2}>>  ex:confidence {3} .
-}} .
-<observation/{0}> prov:generatedAtTime "{1}"^^xsd:dateTime .""".format(i, time, value, 0.9)
+        _:{0} {{
+            <observation/{0}> a sosa:Observation ;
+                sosa:observedProperty <person1/heart_rate> ;
+                sosa:hasFeatureOfInterest <person1> ;
+                sosa:madeBySensor <sensor1> ;
+                sosa:resultTime "{1}"^^xsd:dateTime .
+            <<<observation/{0}> sosa:hasSimpleResult {2}>>  ex:confidence {3} .
+        }} .
+        <observation/{0}> prov:generatedAtTime "{1}"^^xsd:dateTime .""".format(i, time, value, 0.9)
     return test
 
-out = open("heart_rate.trigs", "w")
-prefixes = """@base <http://base/> .
-@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
-@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
-@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
-@prefix foaf: <http://xmlns.com/foaf/0.1/> .
-@prefix sosa: <http://www.w3.org/ns/sosa/> .
-@prefix ssn: <http://www.w3.org/ns/ssn/> .
-@prefix prov: <http://www.w3.org/ns/prov#> .
-@prefix ex: <http:///example.org/ontology#> .
-"""
-out.write(re.sub(r"\s+", " ", prefixes))
-out.write("\n")
+def activity(i, value, t):
+    time = datetime.utcfromtimestamp(t + i).strftime('%Y-%m-%dT%H:%M:%S')
+    test = """
+        _:{0} {{
+            <observation/{0}> a sosa:Observation ;
+                sosa:observedProperty <person1/activity> ;
+                sosa:hasFeatureOfInterest <person1> ;
+                sosa:madeBySensor <sensor2> ;
+                sosa:resultTime "{1}"^^xsd:dateTime .
+            <<<observation/{0}> sosa:hasSimpleResult {2}>>  ex:confidence {3} .
+        }} .
+        <observation/{0}> prov:generatedAtTime "{1}"^^xsd:dateTime .""".format(i, time, value, 0.9)
+    return test
 
-heart_rate_avg = 71
-ref_time = 1556617861
-for i in range(10000): # 1 hour
-    ref_time += 1
-    if i == 60: heart_rate_avg = 120
-    observation = heart_rate(i, heart_rate_avg, ref_time)
-    print(observation)
-    out.write(re.sub(r"\s+", " ", observation))
+def make_heart_rate_stream(prefixes, ref_time):
+    print("Generate stream: heart rate")
+    # init
+    out = open("heart_rate.trigs", "w")
+    out.write(re.sub(r"\s+", " ", prefixes))
     out.write("\n")
-out.close()
+
+    
+    for i in range(40): # 10s
+        # Set start heart rate
+        if i == 0: value = 70
+        # Increase heart rate after 10 s
+        if i == 10: value = 120
+        # Decrease heart rate after 10 s
+        if i == 20: value = 70
+        # Increase heart rate after 10 s
+        if i == 30: value = 120
+
+        observation = heart_rate(i, value, ref_time)
+        out.write(re.sub(r"\s+", " ", observation))
+        out.write("\n")
+    
+    # close file
+    out.close()
+
+def make_activity_stream(prefixes, ref_time):
+    print("Generate stream: activity")
+    # init
+    out = open("activity.trigs", "w")
+    out.write(re.sub(r"\s+", " ", prefixes))
+    out.write("\n")
+
+    
+    for i in range(20): # 10s
+        # Set start activity
+        if i == 0: value = "<activity/resting>"
+        # Change activity
+        if i == 10: value = "<activity/walking>"
+        # Change activity
+        if i == 20: value = "<activity/resting>"
+        # Change activity
+        if i == 30: value = "<activity/walking>"
+
+        observation = activity(i, value, ref_time)
+        out.write(re.sub(r"\s+", " ", observation))
+        out.write("\n")
+    
+    # close file
+    out.close()
+
+def main():
+    prefixes = """
+        @base <http://base/> .
+        @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+        @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+        @prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
+        @prefix foaf: <http://xmlns.com/foaf/0.1/> .
+        @prefix sosa: <http://www.w3.org/ns/sosa/> .
+        @prefix ssn: <http://www.w3.org/ns/ssn/> .
+        @prefix prov: <http://www.w3.org/ns/prov#> .
+        @prefix ex: <http://www.example.org/ontology#> ."""
+    ref_time = 1556617861
+    make_heart_rate_stream(prefixes, ref_time)
+    make_activity_stream(prefixes, ref_time)
 
 
-
-
-
-
-
+main()
 
 
 
