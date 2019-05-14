7c2a721d8cba268d97e63b051868c92917e06b3a
Robin Keskisärkkä <robin.keskisarkka@liu.se>
2019-05-14 08:02:38 +0200

    rename classes for consistency

--- src/main/java/se/liu/ida/rspqlstar/store/dataset/RDFStarStream.java
+++ src/main/java/se/liu/ida/rspqlstar/store/dataset/RDFStarStream.java
@@ -1,33 +1,28 @@
 package se.liu.ida.rspqlstar.store.dataset;
 
 import org.apache.commons.collections4.iterators.IteratorChain;
-import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
 import se.liu.ida.rspqlstar.store.index.IdBasedQuad;
 
-import java.sql.Time;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
-import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class RDFStream {
     final public String iri;
-    final private List<TimestampedGraph> timestampedGraphs = new ArrayList<>();
+    final private List<RDFStarStreamElement> timestampedGraphs = new ArrayList<>();
 
     public RDFStream(String iri){
         this.iri = iri;
     }
 
-    public void push(TimestampedGraph timestampedGraph){
+    public void push(RDFStarStreamElement timestampedGraph){
         timestampedGraphs.add(timestampedGraph);
     }
 
     public Iterator<IdBasedQuad> iterator(long lowerBound, long upperBound){
         final IteratorChain<IdBasedQuad> iteratorChain = new IteratorChain<>();
         for(int i=0; i < timestampedGraphs.size(); i++){
-            final TimestampedGraph tg = timestampedGraphs.get(i);
+            final RDFStarStreamElement tg = timestampedGraphs.get(i);
             if(lowerBound > tg.time) continue;
             if(upperBound <= tg.time) break;
             iteratorChain.addIterator(tg.iterateAll());
@@ -35,9 +30,9 @@ public class RDFStream {
         return iteratorChain;
     }
 
-    public List<TimestampedGraph> iterateElements(long lowerBound, long upperBound){
-        final List<TimestampedGraph> tgs = new ArrayList<>();
-        for(TimestampedGraph tg : timestampedGraphs){
+    public List<RDFStarStreamElement> iterateElements(long lowerBound, long upperBound){
+        final List<RDFStarStreamElement> tgs = new ArrayList<>();
+        for(RDFStarStreamElement tg : timestampedGraphs){
             if(lowerBound > tg.time) continue;
             if(upperBound <= tg.time) break;
             tgs.add(tg);
